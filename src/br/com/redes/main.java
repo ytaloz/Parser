3d191ae8469e5435329adef0d66999596e3f10dd
Italo Silva <ytaloz@hotmail.com>
2015-03-24 23:00:42 -0300

    Ajuste

--- src/br/com/redes/main.java
+++ src/br/com/redes/main.java
@@ -7,7 +7,7 @@ import br.com.redes.Controller.Parser;
 
 public class main {
 	public static void main(String[] args) throws IOException{
-		Parser parser = new Parser("http://mjb.barefamily.org/johnny/index.html");
+		Parser parser = new Parser("http://sites.ecomp.uefs.br/joao/home/courses/exa844");
 		parser.filtrarCodigo();
 		System.out.println("Titulo da pagina: " + parser.getTitle());
 		System.out.println("Texto da pagina: " + parser.getPagina().getTexto());
